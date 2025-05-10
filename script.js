876ffdbeb97f79eb8419e91cd67e10140ee164cc
Mishraji <151707131+CoderwithMishraji@users.noreply.github.com>
2025-05-11 01:13:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Every heartbeat of mine is stitched by your love."], // Apne khud ke words daal
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop: true
+});
